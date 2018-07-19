e62bc7642ad9d5645bf49aa93dd782f304962e32
meem <meem@ubuntu>
2018-07-19 14:28:35 +0900

    more accurate

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -4,9 +4,30 @@ public class Calendar {
 	private static final int[] Max_Days = {31,28,31,30,31,30,31,31,30,31,30,31};
 	private static final int[] Leap_Max_Days = {31,29,31,30,31,30,31,31,30,31,30,31};
 	
-	private boolean isLeap(int year) {
+	public boolean isLeap(int year) {
 		if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {return true;}
 		else {return false;}
+	}
+	
+	public int parseDay(String weekday) {
+		switch(weekday) {
+		case("Su"):
+			return 1;
+		case("Mo"):
+			return 2;
+		case("Tu"):
+			return 3;
+		case("We"):
+			return 4;
+		case("Th"):
+			return 5;
+		case("Fr"):
+			return 6;
+		case("Sa"):
+			return 7;
+		default:
+			return 1;
+		}
 	}
 	
 	public int getMaxDays(int year, int month) {
@@ -14,13 +35,18 @@ public class Calendar {
 		else {return Max_Days[month-1];}
 	}
 	
-	public void printCal(int year, int month, int day) {
+	public void printCal(int year, int month, int weekday) {
+		int days = getMaxDays(year, month);
 		System.out.printf("    %4d년%3d월\n", year,month);
 		System.out.println("---------------------");
 		System.out.println("Su Mo Tu We Th Fr Sa");
-		for(int i=1;i<=day;i++) {
-			System.out.printf("%3d",i);
-			if (i%7 == 0) {System.out.print("\n");}
+		for(int i=1;i<weekday;i++) {
+			System.out.printf("%3s"," ");
+		}
+		for(int j=1;j<=days;j++) {
+			System.out.printf("%3d",j);
+			if (j%7 == 0 && j%7 == 7-(weekday-1)) {System.out.print("\n");}
+			if (j%7 == 7-(weekday-1)) {System.out.print("\n");}
 		}
 		System.out.print("\n");
 	}
--- src/calendar/prompt.java
+++ src/calendar/prompt.java
@@ -9,16 +9,17 @@ public class prompt {
 		
 		int month = 1;
 		int year = 1;
-		int day = 1;
+		int weekday = 1;
 		while(true) {
 			System.out.print("year: ");
 			year = sc.nextInt();
 			System.out.print("month: ");
 			month = sc.nextInt();
+			System.out.print("start: ");
+			weekday = cal.parseDay(sc.next());
 			if(month == -1) {break;}
 			if(month>12) {continue;}
-			day = cal.getMaxDays(year, month);
-			cal.printCal(year, month, day);
+			cal.printCal(year, month, weekday);
 		}
 		System.out.println("End");
 		sc.close();
